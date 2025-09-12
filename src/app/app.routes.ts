f0b0f5b1e6a9d026f24099907a65854a7094ab4f
sofidor <sofiadorbe@gmail.com>
2025-09-12 19:35:25 -0300

    Cambios Form Cliente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { loginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-
-import { Tab1Page } from './tab1/tab1.page';
 import { HomeComponent } from './home/tab2.page';
-
+import { RegistroClienteAnonimoComponent } from './components/registro-cliente-anonimo/registro-cliente-anonimo.component';
+import { RegistroClienteComponent } from './components/registro-cliente/registro-cliente.component';
 
 export const routes: Routes = [
   {
@@ -23,5 +22,15 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {path: 'home',component: HomeComponent,pathMatch: 'full',},
+    {
+    path: 'registro-cliente',
+    component: RegistroClienteComponent,
+    pathMatch: 'full',
+  },
+    {
+    path: 'registro-cliente-anonimo',
+    component: RegistroClienteAnonimoComponent,
+    pathMatch: 'full',
+  }
 
 ];
